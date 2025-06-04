9c8f1211e8816bd4596717946bd41441276468ab
Muhammad Zaki Jauhari <mzakijauhari@gmail.com>
2025-06-04 22:51:04 +0700

    configurasi hosting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/MuhammadZakiJauhari-233510607-Tugas2/', 
 })
+
+
